6d3e45a4c27457c58e75e89de3b6d2690e450fa8
Ruben S Sevaldson <rubenss@stud.ntnu.no>
2019-02-19 21:36:53 +0100

    added sudo

--- setup.sh
+++ setup.sh
@@ -1,38 +1,37 @@
 #!/bin/sh
 
-apt-get update
-apt-get --assume-yes upgrade
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
 
 # Install essentials
-apt-get --assume-yes install git
-apt-get --assume-yes install vim
-apt-get --assume-yes install curl
-apt-get --assume-yes install wget
-apt-get --assume-yes install thunderbird
+sudo apt-get --assume-yes install git
+sudo apt-get --assume-yes install vim
+sudo apt-get --assume-yes install curl
+sudo apt-get --assume-yes install wget
+sudo apt-get --assume-yes install thunderbird
 
 # Install neovim
-apt-get --assume-yes install software-properties-common
-add-apt-repository ppa:neovim-ppa/stable
-apt-get update
-apt-get --assume-yes install neovim
-apt-get --assume-yes install python-dev python-pip python3-dev python3-pip
+sudo apt-get --assume-yes install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get --assume-yes install neovim
+sudo apt-get --assume-yes install python-dev python-pip python3-dev python3-pip
 
 # Make config file and directory and get custom nvim config file
 echo "Installing neovim"
-mkdir ~/.config/nvim
-touch ~/.config/nvim/init.vim
+sudo mkdir ~/.config/nvim
+sudo touch ~/.config/nvim/init.vim
 echo "curl'ing custom nvim config"
-curl https://raw.githubusercontent.com/rubensseva/vim_configs/master/init.vim > ~/.config/nvim/init.vim
+sudo curl https://raw.githubusercontent.com/rubensseva/vim_configs/master/init.vim > ~/.config/nvim/init.vim
 # Install vim-plug for neovim
 echo "curl'ing neovim"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install nodejs
 echo "curl'ing nodejs"
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get --assume-yes install -y nodejs
+sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo sudo apt-get --assume-yes install -y nodejs
 
 # Install zsh and oh-my-zsh
-apt-get --assume-yes install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get --assume-yes install zsh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
