a76d0fb0ca2df7b234b575310303bf74db40b941
Ruben <36086770+rubensseva@users.noreply.github.com>
2019-03-06 19:34:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,16 @@ sudo apt-get update
 sudo apt-get --assume-yes upgrade
 
 # Install essentials
+sudo apt-get --assume-yes install apt-transport-https
 sudo apt-get --assume-yes install git
 sudo apt-get --assume-yes install vim
 sudo apt-get --assume-yes install curl
 sudo apt-get --assume-yes install wget
 sudo apt-get --assume-yes install thunderbird
+
+# Install zsh and oh-my-zsh
+sudo apt-get --assume-yes install zsh
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install fzf
 # Probably needs to be done before vim config file is used
@@ -38,8 +43,16 @@ echo "curl'ing nodejs"
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo sudo apt-get --assume-yes install -y nodejs
 
-# Install zsh and oh-my-zsh
-sudo apt-get --assume-yes install zsh
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Install vscode
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+# Install sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get install sublime-text
+
+
 
 
