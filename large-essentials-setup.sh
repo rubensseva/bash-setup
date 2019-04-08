cdf44f2a156b1f4cb9fc1cba6a605774b25a0803
Ruben <36086770+rubensseva@users.noreply.github.com>
2019-04-08 22:06:52 +0200

    Update large-essentials-setup.sh

--- large-essentials-setup.sh
+++ large-essentials-setup.sh
@@ -6,7 +6,7 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt --assume-yes install nodejs
 
 # Install java 11
-sudo add-apt-repositor -y ppa:linux
+sudo add-apt-repository -y ppa:linuxuprising/java
 sudo apt update
-sudo apt install -y oracle-java11-installeruprising/java
+sudo apt install -y oracle-java11-installer
 sudo apt install oracle-java11-set-default
